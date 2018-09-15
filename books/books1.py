4126e3b799968d4d9af327aa2d18c04ae277434f
bat-orgil <batjargalb@carleton.edu>
2018-09-15 14:37:23 -0500

    Loaded the file into a list

--- books/books1.py
+++ books/books1.py
@@ -1,3 +1,11 @@
 import csv
 import sys
 
+with open('books.csv', newline='') as f: #why do we need newLine= here? 
+    reader = csv.reader(f)
+    everyLine = []
+    for row in reader:
+    	wordReader = csv.reader()
+        everyLine.append(row)
+    for line in everyLine:
+    	print (line)
