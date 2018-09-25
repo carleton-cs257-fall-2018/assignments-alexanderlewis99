f35dd5ad4141ed7a8f22055b6d5d24c2c968c7a6
bat-orgil <batjargalb@carleton.edu>
2018-09-25 16:48:01 -0500

    bugs are there

--- books/booksdatasource.py
+++ books/booksdatasource.py
@@ -76,8 +76,8 @@ class BooksDataSource:
             NOTE TO STUDENTS: I have not specified how you will store the books/authors
             data in a BooksDataSource object. That will be up to you, in Phase 3.
         '''
-        self.books = []
-        self.authors = []
+        self.books_data = []
+        self.authors_data = []
         self.maps = []
 
         books_file = open(books_filename, 'r')
@@ -93,7 +93,7 @@ class BooksDataSource:
             book_title = book_line[1]
             book_publication_year = book_line[2]
             book = {'id': book_id, 'title': book_title, 'publication_year': book_publication_year}
-            self.books.append(book)
+            self.books_data.append(book)
         for author_line in authors_reader:
             author_id = author_line[0]
             author_last_name = author_line[1]
@@ -102,7 +102,7 @@ class BooksDataSource:
             author_death_year = author_line[4]
             author = {'id': author_id, 'last_name': author_last_name, 'first_name': author_first_name,
                       'birth_year': author_birth_year, 'death_year': author_death_year}
-            self.authors.append(author)
+            self.authors_data.append(author)
         for line in books_authors_reader:
             book_id = line[0]
             author_id = line[1]
@@ -119,7 +119,7 @@ class BooksDataSource:
             raise TypeError('Invalid type for book ID.')
 
         book_to_return = None
-        for book in self.books:
+        for book in self.books_data:
             if int(book['id']) == book_id:
                 book_to_return = book
         if book_to_return == None:
@@ -153,18 +153,20 @@ class BooksDataSource:
             QUESTION: How about ValueError? And if so, for which parameters?
             Raises ValueError if author_id is non-None but is not a valid author ID.
         '''
-        books_matching_search_query = self.books
+        books_matching_search_query = self.books_data
         if(not(author_id is None or (type(author_id)==int and author_id >= 0))
         or not(search_text is None or type(search_text)==str)
         or not(start_year is None or type(author_id)==int)
         or not(end_year is None or type(author_id)==int)):
             raise TypeError('Invalid parameter types')
 
-        author_ids = []
-        for book in self.books:
-            author_ids.append(book['author_id'])
-        if not author_id in author_ids:
-            raise ValueError("Invalid author_id")
+
+        if not author_id is None:
+            author_ids = []
+            for author in self.authors_data:
+                author_ids.append(author['id'])
+            if not author_id in author_ids:
+                raise ValueError("Invalid author_id")
 
         if not (start_year is None and end_year is None):
             if (start_year > end_year):
@@ -173,7 +175,7 @@ class BooksDataSource:
         if not sort_by.lower() in ('title', 'year'):
             raise ValueError("Invalid sorting method")
 
-        for book in self.books:
+        for book in self.books_data:
             book_removed = False
             if not author_id == None:
                 for book_author_pair in self.map:
@@ -211,7 +213,7 @@ class BooksDataSource:
             raise TypeError('Invalid type for author ID.')
 
         author_to_return = None
-        for author in self.authors:
+        for author in self.authors_data:
             if int(author['id']) == author_id:
                 author_to_return = author
         if author_to_return == None:
@@ -244,7 +246,7 @@ class BooksDataSource:
             See the BooksDataSource comment for a description of how an author is represented.
         '''
 
-        authors_matching_search_query = self.authors
+        authors_matching_search_query = self.authors_data
 
         if(not(author_id is None or (type(author_id)==int and author_id >= 0))
         or not(search_text is None or type(search_text)==str)
@@ -252,11 +254,13 @@ class BooksDataSource:
         or not(end_year is None or type(author_id)==int)):
             raise TypeError('Invalid parameter types')
 
-        author_ids = []
-        for book in self.books:
-            author_ids.append(book['author_id'])
-        if not author_id in author_ids:
-            raise ValueError("Invalid author_id")
+
+        if not book_id is None:
+            book_ids = []
+            for book in self.books_data:
+                book_ids.append(book['id'])
+            if not book_id in book_ids:
+                raise ValueError("Invalid book_id")
 
         if not (start_year is None and end_year is None):
             if (start_year > end_year):
@@ -266,7 +270,7 @@ class BooksDataSource:
             raise ValueError("Invalid sorting method")
 
 
-        for author in self.authors:
+        for author in self.authors_data:
             author_removed = False
             if not book_id == None:
                 for book_author_pair in self.map:
