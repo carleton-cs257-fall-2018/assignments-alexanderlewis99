96336df936423cdd05c8787ee1afe0d7505e71a9
“Alec <alewang99@gmail.com>
2018-10-14 15:53:45 -0500

    adds csv files to repo

---
+++ webapp/api-test.py
@@ -0,0 +1,15 @@
+1: Petroleum Engineering
+2: Mining And Mineral Engineering
+3: Metallurgical Engineering
+4: Naval Architecture And Marine Engineering
+5: Chemical Engineering
+20: Court reporting
+30: Public Policy
+42: Mathematics
+48: Applied Mathematics
+53: Mathematics and Computer Science
+75: Chemistry
+83: Biochemical Sciences
+88: Pre-law and Legal Studies
+90: Public Administration
+95: Criminal Justice and Fire Protection
