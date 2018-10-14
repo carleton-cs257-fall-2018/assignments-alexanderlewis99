7192071fd493a4ab612d07e3ec62c4549d76ce0b
bat-orgil <batjargalb@carleton.edu>
2018-10-14 17:54:52 -0500

    method names and routes are organized

---
+++ webapp/methods.py
@@ -0,0 +1,43 @@
+import 
+
+
+#methods that we will use on the api - CareerSalaryDataSource
+@app.route(/majors)
+def get_majors()
+
+	#Get a list of all the undergraduate majors
+	#try to get the data and loop and add them into a list then print
+
+@app.route(/majors/<category>)
+def get_majors_in_catergory(<category>)
+	
+	#Get a list of all the undergraduate majors in a particular category
+
+	limit = flask.request.args.get('limit')
+
+@app.route(/majors/<category>/<program>)
+def get_majors_in_program_category(<category>, <program>)
+	limit = flask.request.args.get('limit')
+	#Get a list of all the undergraduate majors in a particular program in a particular category
+
+@app.route(/majors/null/null/<minimum_salary>)
+def get_majors_by_minimum_salary(<minimum_salary>)
+	limit = flask.request.args.get('limit')
+	#Get a list of all the undergraduate majors that make a particular minimum salary after they graduate
+
+@app.route(majors/<category>/null/<minimum_salary>)
+
+def get_majors_by_minimumSalary_in_category(<category>, <minimum_salary>)
+	limit = flask.request.args.get('limit')	
+
+	#Get a list of all the undergraduate majors that make a particular minimum salary after they graduate in a particular category
+@app.route(majors/null/<program>/<minimum_salary>)
+def get_majors_by_minimum_salary_in_program(<program>, <minimum_salary>)
+	limit = flask.request.args.get('limit')
+
+	#Get a list of all the undergraduate majors that make a particular minimum salary after they graduate in a particular major from any category
+
+@app.route(majors/<category>/<program>/<minimum_salary>)
+def get_majors_by_minimum_salary_in_program_in_category(<category>,<program>,<minimum_salary>)
+	limit = flask.request.args.get('limit')
+	#Get a list of all the undergraduate majors that make a particular minimum salary after they graduate in a particular major from particular category
