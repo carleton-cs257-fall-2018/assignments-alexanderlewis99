3070a0175254808bbdf13f4e16eadf1d220865ca
“Alec <alewang99@gmail.com>
2018-10-16 15:44:29 -0500

    completes the converter programs

--- webapp/csv_converter.py
+++ webapp/csv_converter.py
@@ -10,29 +10,33 @@ import sys
 import re
 import csv
 
-def load_from_majors_csv_file(recent_grads_csv, category_numbers_csv):
+def get_category_ids(category_ids_csv):
+    #opens category_numbers_csv
+    category_ids_csv_file = open(category_ids_csv)
+    category_ids_csv_reader = csv.reader(category_ids_csv_file)
+    category_ids = {}
+    for row in category_ids_csv_reader:
+        category = row[1]
+        id = row[0]
+        if category not in category_ids.keys():
+            category_ids[category] = id
+    category_ids_csv_file.close()
+    return(category_ids)
+
+def write_recent_grads_csv_with_category_ids(recent_grads_csv, category_ids):
     #opens recent_grads_csv
     majors_csv_file = open(recent_grads_csv)
     majors_csv_reader = csv.reader(majors_csv_file)
-
     #opens output_file.csv
-    output_file = open("output_file.csv", 'w')
+    output_file = open("recent-grads-category-ids.csv", 'w')
     writer = csv.writer(output_file)
-
-    #opens category_numbers_csv
-    category_numbers_csv_file = open(category_numbers_csv)
-    category_numbers_csv_reader = csv.reader(category_numbers_csv_file)
-
-    category_numbers = {}
-    for row in category_numbers_csv_reader:
-        if row[0] not in category_numbers.keys():
-            category_numbers[row[1]] = row[0]
-    print(category_numbers)
-
     for row in majors_csv_reader:
-        new_row = [category_numbers[row[6]], row[6]]
+        new_row = row
+        new_row[6] = category_ids[row[6]]
         writer.writerow(new_row)
     majors_csv_file.close()
     output_file.close()
 
-load_from_majors_csv_file("recent-grads.csv", "category_numbers.csv")
+if __name__ == '__main__':
+    category_ids = get_category_ids("category_ids.csv")
+    write_recent_grads_csv_with_category_ids("recent-grads.csv", category_ids)
--- webapp/csv_converter_produce_id_category.py
+++ webapp/csv_converter_produce_id_category.py
@@ -1,9 +1,9 @@
 """
 Converter 1:
-Funtions: 
-1.Take a csv 
-2.produce a simple csv with one collumn from the original csv 
-Bat-Orgil Batjargal 
+Funtions:
+1.Take a csv
+2.produce a simple csv with one collumn from the original csv
+Bat-Orgil Batjargal
 Oct-15, 2018
 
 """
@@ -13,37 +13,39 @@ import re
 import csv
 
 
-def load_from_books_csv_file(csv_file_name):
+def get_category_ids_from_recent_grads_csv_file(csv_file_name):
     csv_file = open(csv_file_name, encoding='utf-8')
     reader = csv.reader(csv_file)
-
     category = {}
     ids_categories = []
+    added_categories = []
     for row in reader:
         assert len(row) == 21
-
-        if row[6] not in ids_categories.values():
+        if row[6] == 'Major_category':
+            continue
+        if row[6] not in added_categories:
             new_id = len(ids_categories)
             category = {'id': new_id, 'name': row[6]}
-            print(category)
             ids_categories.append(category)
+            added_categories.append(row[6])
     csv_file.close()
     return (ids_categories)
 
-def save_linking_table(ids_categories, csv_file_name):
-    ''' Save the books in CSV form, with each row containing
-        (book id, author id). '''
+def save_category_id_table(ids_categories, csv_file_name):
+    ''' Save the category_ids in CSV form, with each row containing
+        (category id, category name). '''
     output_file = open(csv_file_name, 'w', encoding='utf-8')
     writer = csv.writer(output_file)
+    first_row = {'Major_category', 'category_id'}
+    writer.writerow(first_row)
     for id_category in ids_categories:
         id_category_row = [id_category['id'], id_category['name']]
         writer.writerow(id_category_row)
     output_file.close()
 
-
 if __name__ == '__main__':
     print("hi")
-    ids_categories = load_from_books_csv_file('recent-grads.csv')
+    ids_categories = get_category_ids_from_recent_grads_csv_file('recent-grads.csv')
     print("Yow")
-    save_linking_table(ids_categories, 'books_authors.csv')
+    save_category_id_table(ids_categories, 'category_ids.csv')
     print("finished")
