fdbc136bda88959ea5ab49bbcee1449c75f9ef94
“Alec <alewang99@gmail.com>
2018-10-21 15:50:05 -0500

    makes cursor

--- webapp/api.py
+++ webapp/api.py
@@ -24,12 +24,13 @@ app = flask.Flask(__name__)
 def get_majors(category_id = None, minimum_salary = None, major_contains = None, sort_by = None, limit = None):
 	arguments = get_url_query_string_args(category_id, minimum_salary, major_contains, sort_by, limit)
 	sql_query_requirements = get_query_requirements(arguments)
-	if (len(sql_query_requirements) > 0):
+
+    if (len(sql_query_requirements) > 0):
 		sql_query = 'SELECT * FROM majors WHERE ' + query_requirements
 	else:
 		sql_query = 'SELECT * FROM majors'
-
 	try:
+        cursor = connection.cursor()
 	    cursor.execute(sql_query)
 	except Exception as e:
 	    print(e)
