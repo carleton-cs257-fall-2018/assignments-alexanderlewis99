e95c6f1308271249bc3ce5e1b7685af4b36d62b4
“Alec <alewang99@gmail.com>
2018-10-30 21:30:38 -0500

    changes styles of the help text

--- webapp/static/majors.js
+++ webapp/static/majors.js
@@ -146,9 +146,9 @@ function toggle_help(){
     help_button_html += "<li>Physical Sciences</li>";
     help_button_html += "<li>Psychology & Social Work</li>";
     help_button_html += "<li>Social Science</li>";
-    help_button_html += "</ol><br><br>";
-    help_button_html += "Major: Search text used to find majors. <br>";
-    help_button_html += "Minimum Salary: Filters results so that they include only majors whose median salaries match or exceed the min salary.<br>";
+    help_button_html += "</ol>";
+    help_button_html += "Major: Search text used to find majors. <br><br>";
+    help_button_html += "Minimum Salary: Filters results so that they include only majors whose median salaries match or exceed the min salary.<br><br>";
     help_button_html += "Sort By: Sorts the results by the element in descending order.<br>";
   }
   else{
